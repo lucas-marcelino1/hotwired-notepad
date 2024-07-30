57f6fecd3415d8e49e22acb5cdc3593fe68c2564
Lucas Marcelino <lucas_marcelino2000@hotmail.com>
2024-07-30 16:10:24 -0300

    Add edit/update and some css styles

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class NotesController < ApplicationController
-  before_action :find_note, only: :destroy
+  before_action :find_note, only: %i[destroy edit update]
   def index
-    @notes = Note.order(:created_at)
+    @notes = Note.order(created_at: :desc)
   end
 
   def new
@@ -29,7 +29,20 @@ class NotesController < ApplicationController
         format.turbo_stream
       end
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @note.update(note_params)
+      respond_to do |format|
+        format.html { redirect_to root_path, notice: "Note was successfully edited!" }
+        format.turbo_stream
+      end
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
